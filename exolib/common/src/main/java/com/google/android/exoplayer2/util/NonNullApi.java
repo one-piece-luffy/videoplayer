86c18e4eae9c2aa1c45ecb9ba486317d1ce9f03e
lhy <646796992@qq.com>
2021-09-26 17:54:49 +0800

    2

--- exolib/common/src/main/java/com/google/android/exoplayer2/util/NonNullApi.java
+++ exolib/common/src/main/java/com/google/android/exoplayer2/util/NonNullApi.java
@@ -20,8 +20,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import javax.annotation.Nonnull;
 import javax.annotation.meta.TypeQualifierDefault;
-import kotlin.annotations.jvm.MigrationStatus;
-import kotlin.annotations.jvm.UnderMigration;
 
 /**
  * Annotation to declare all type usages in the annotated instance as {@link Nonnull}, unless
@@ -29,6 +27,5 @@ import kotlin.annotations.jvm.UnderMigration;
  */
 @Nonnull
 @TypeQualifierDefault(ElementType.TYPE_USE)
-@UnderMigration(status = MigrationStatus.STRICT)
 @Retention(RetentionPolicy.CLASS)
 public @interface NonNullApi {}
